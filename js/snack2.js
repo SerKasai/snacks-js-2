15c710df3bd962f9c67fe77a7217e9fca916a823
SerKasai <serjtheignis92@gmail.com>
2025-06-12 16:03:22 +0200

    aggiunta alternativa snack 2

--- js/snack2.js
+++ js/snack2.js
@@ -6,4 +6,26 @@ const array2 = array1.map((el) => {
   return el.toLowerCase(array1);
 })
 
-console.log(array1, array2)
+// console.log(array1, array2)
+
+
+// ALTERNATIVA
+
+// function capitalize(word) {
+
+//   let capitalizedWord = word.toLowerCase();
+
+//   let firstLetter = capitalizedWord.charAt(0).toUpperCase();
+//   console.log(firstLetter);
+
+//   capitalizedWord = firstLetter + capitalizedWord.slice(1);
+
+//   return capitalizedWord;
+// }
+
+// const capitalizedArray = array1.map(nome => {
+
+//   return capitalize(nome);
+// })
+
+// console.log(array1, capitalizedArray)
